a879acad11225f53c0f2a7860b96cf5a1147dc97
Aeternia <gherasymuk@gmail.com>
2015-01-31 18:01:06 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -8,14 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Example {
-
-	protected WebDriver webDriver;
-
-	@BeforeMethod
-	public void setup() {
-		setChromeDriver();
-		webDriver = new ChromeDriver();
+fdkdffjkvkjngbkjfbknb
 	}
 
 	@AfterMethod
